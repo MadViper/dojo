46899e4c821d0c24f8a9aa07824725bbf42d2ec9
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2023-07-04 20:48:43 +0400

    Rename

--- mobsession/yahtzee/yatzy.py
+++ mobsession/yahtzee/yatzy.py
@@ -7,9 +7,8 @@ class Yatzy:
 
     def yatzy(self) -> int:
         result = 0
-        counts = self.histogram()
-        for i in range(len(counts)):
-            if counts[i] == 5:
+        for i in range(len(self.histogram)):
+            if self.histogram[i] == 5:
                 result = 50
 
         return result
@@ -79,19 +78,17 @@ class Yatzy:
 
     def one_pair(self):
         result = 0
-        counts = self.histogram()
         for at in range(6):
-            if counts[6 - at - 1] == 2:
+            if self.histogram[6 - at - 1] == 2:
                 result = (6 - at) * 2
                 break
         return result
 
     def two_pairs(self):
-        counts = self.histogram()
         n = 0
         score = 0
         for i in range(6):
-            if counts[6 - i - 1] >= 2:
+            if self.histogram[6 - i - 1] >= 2:
                 n = n + 1
                 score += 6 - i
         if n == 2:
@@ -102,13 +99,13 @@ class Yatzy:
 
     def four_of_a_knd(self):
         result = 0
-        counts = self.histogram()
         for i in range(6):
-            if counts[i] >= 4:
+            if self.histogram[i] >= 4:
                 result = (i + 1) * 4
 
         return result
 
+    @property
     def histogram(self) -> [int]:
         result = [0] * 6
         for die in self.dice:
@@ -118,22 +115,20 @@ class Yatzy:
 
     def three_of_a_kind(self):
         result = 0
-        counts = self.histogram()
         for i in range(6):
-            if counts[i] >= 3:
+            if self.histogram[i] >= 3:
                 result = (i + 1) * 3
 
         return result
 
     def small_straight(self):
         result = 0
-        counts = self.histogram()
         if (
-            counts[0] == 1
-            and counts[1] == 1
-            and counts[2] == 1
-            and counts[3] == 1
-            and counts[4] == 1
+            self.histogram[0] == 1
+            and self.histogram[1] == 1
+            and self.histogram[2] == 1
+            and self.histogram[3] == 1
+            and self.histogram[4] == 1
         ):
             result = 15
 
@@ -141,40 +136,28 @@ class Yatzy:
 
     def large_straight(self):
         result = 0
-        counts = self.histogram()
         if (
-            counts[1] == 1
-            and counts[2] == 1
-            and counts[3] == 1
-            and counts[4] == 1
-            and counts[5] == 1
+            self.histogram[1] == 1
+            and self.histogram[2] == 1
+            and self.histogram[3] == 1
+            and self.histogram[4] == 1
+            and self.histogram[5] == 1
         ):
             result = 20
         return result
 
     def full_house(self):
-        d_ = self.dice[0]
-        d_1 = self.dice[1]
-        d_2 = self.dice[2]
-        d_3 = self.dice[3]
-        d_4 = self.dice[4]
         result = 0
         _2 = False
         _2_at = 0
         _3 = False
         _3_at = 0
-        tallies = [0] * 6
-        tallies[d_ - 1] += 1
-        tallies[d_1 - 1] += 1
-        tallies[d_2 - 1] += 1
-        tallies[d_3 - 1] += 1
-        tallies[d_4 - 1] += 1
         for i in range(6):
-            if tallies[i] == 2:
+            if self.histogram[i] == 2:
                 _2 = True
                 _2_at = i + 1
         for i in range(6):
-            if tallies[i] == 3:
+            if self.histogram[i] == 3:
                 _3 = True
                 _3_at = i + 1
         if _2 and _3:
