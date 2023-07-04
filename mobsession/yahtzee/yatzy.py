f19c020a928f7528192e01a8cd801ed9fd4880b3
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2023-07-04 19:02:04 +0400

    Rearrange

--- mobsession/yahtzee/yatzy.py
+++ mobsession/yahtzee/yatzy.py
@@ -1,4 +1,12 @@
 class Yatzy:
+    def __init__(self, d1, d2, d3, d4, _5):
+        self.dice = [0] * 5
+        self.dice[0] = d1
+        self.dice[1] = d2
+        self.dice[2] = d3
+        self.dice[3] = d4
+        self.dice[4] = _5
+
     @staticmethod
     def chance(d1, d2, d3, d4, d5):
         total = 0
@@ -64,14 +72,6 @@ class Yatzy:
         if d5 == 3:
             s += 3
         return s
-
-    def __init__(self, d1, d2, d3, d4, _5):
-        self.dice = [0] * 5
-        self.dice[0] = d1
-        self.dice[1] = d2
-        self.dice[2] = d3
-        self.dice[3] = d4
-        self.dice[4] = _5
 
     def fours(self):
         sum = 0
