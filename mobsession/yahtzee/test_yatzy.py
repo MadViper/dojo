bd37574de78ff1d7c87c5151b371c1aafed3d56c
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2023-07-04 18:57:38 +0400

    Fix test names

--- mobsession/yahtzee/test_yatzy.py
+++ mobsession/yahtzee/test_yatzy.py
@@ -12,14 +12,14 @@ def test_yatzy_scores_50():
     assert Yatzy.yatzy([6, 6, 6, 6, 3]) == 0
 
 
-def test_1s():
+def test_ones():
     assert Yatzy.ones(1, 2, 3, 4, 5) == 1
     assert Yatzy.ones(1, 2, 1, 4, 5) == 2
     assert Yatzy.ones(6, 2, 2, 4, 5) == 0
     assert Yatzy.ones(1, 2, 1, 1, 1) == 4
 
 
-def test_2s():
+def test_twos():
     assert Yatzy.twos(1, 2, 3, 2, 6) == 4
     assert Yatzy.twos(2, 2, 2, 2, 2) == 10
 
@@ -29,7 +29,7 @@ def test_threes():
     assert Yatzy.threes(2, 3, 3, 3, 3) == 12
 
 
-def test_fours_test():
+def test_fours():
     assert Yatzy(4, 4, 4, 5, 5).fours() == 12
     assert Yatzy(4, 4, 5, 5, 5).fours() == 8
     assert Yatzy(4, 5, 5, 5, 5).fours() == 4
@@ -41,7 +41,7 @@ def test_fives():
     assert Yatzy(4, 5, 5, 5, 5).fives() == 20
 
 
-def test_sixes_test():
+def test_sixes():
     assert Yatzy(4, 4, 4, 5, 5).sixes() == 0
     assert Yatzy(4, 4, 6, 5, 5).sixes() == 6
     assert Yatzy(6, 5, 6, 6, 5).sixes() == 18
@@ -53,7 +53,7 @@ def test_one_pair():
     assert Yatzy.score_pair(5, 3, 6, 6, 5) == 12
 
 
-def test_two_Pair():
+def test_two_pair():
     assert Yatzy.two_pair(3, 3, 5, 4, 5) == 16
     assert Yatzy.two_pair(3, 3, 6, 6, 6) == 18
     assert Yatzy.two_pair(3, 3, 6, 5, 4) == 0
@@ -72,18 +72,18 @@ def test_four_of_a_knd():
     assert Yatzy.four_of_a_kind(3, 3, 3, 2, 1) == 0
 
 
-def test_smallStraight():
+def test_small_straight():
     assert Yatzy.smallStraight(1, 2, 3, 4, 5) == 15
     assert Yatzy.smallStraight(2, 3, 4, 5, 1) == 15
     assert Yatzy.smallStraight(1, 2, 2, 4, 5) == 0
 
 
-def test_largeStraight():
+def test_large_straight():
     assert Yatzy.largeStraight(6, 2, 3, 4, 5) == 20
     assert Yatzy.largeStraight(2, 3, 4, 5, 6) == 20
     assert Yatzy.largeStraight(1, 2, 2, 4, 5) == 0
 
 
-def test_fullHouse():
+def test_full_house():
     assert Yatzy.fullHouse(6, 2, 2, 2, 6) == 18
     assert Yatzy.fullHouse(2, 3, 4, 5, 6) == 0
