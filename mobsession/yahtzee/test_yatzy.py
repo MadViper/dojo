1e802d0a40e96e3091fc57b9b8e99b8cd4e6f970
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2023-07-04 18:56:18 +0400

    Flip expected and actual values

--- mobsession/yahtzee/test_yatzy.py
+++ mobsession/yahtzee/test_yatzy.py
@@ -30,21 +30,21 @@ def test_threes():
 
 
 def test_fours_test():
-    assert 12 == Yatzy(4, 4, 4, 5, 5).fours()
-    assert 8 == Yatzy(4, 4, 5, 5, 5).fours()
-    assert 4 == Yatzy(4, 5, 5, 5, 5).fours()
+    assert Yatzy(4, 4, 4, 5, 5).fours() == 12
+    assert Yatzy(4, 4, 5, 5, 5).fours() == 8
+    assert Yatzy(4, 5, 5, 5, 5).fours() == 4
 
 
 def test_fives():
-    assert 10 == Yatzy(4, 4, 4, 5, 5).fives()
-    assert 15 == Yatzy(4, 4, 5, 5, 5).fives()
-    assert 20 == Yatzy(4, 5, 5, 5, 5).fives()
+    assert Yatzy(4, 4, 4, 5, 5).fives() == 10
+    assert Yatzy(4, 4, 5, 5, 5).fives() == 15
+    assert Yatzy(4, 5, 5, 5, 5).fives() == 20
 
 
 def test_sixes_test():
-    assert 0 == Yatzy(4, 4, 4, 5, 5).sixes()
-    assert 6 == Yatzy(4, 4, 6, 5, 5).sixes()
-    assert 18 == Yatzy(6, 5, 6, 6, 5).sixes()
+    assert Yatzy(4, 4, 4, 5, 5).sixes() == 0
+    assert Yatzy(4, 4, 6, 5, 5).sixes() == 6
+    assert Yatzy(6, 5, 6, 6, 5).sixes() == 18
 
 
 def test_one_pair():
