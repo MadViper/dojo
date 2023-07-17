28e60041d6968d66f93f150f9e8046ff7f342f5f
MadViper1994 <mikheil.kenchoshvili@gmail.com>
2023-07-17 15:54:26 +0400

    Green: fake the implementation

--- kata/test_decimal_to_binary.py
+++ kata/test_decimal_to_binary.py
@@ -1,5 +1,9 @@
 # TODO: Given a decimal integer convert it to binary string of 16 bits.
 
 
+def decimal_to_binary(number: int) -> str:
+    return "0000000000000000"
+
+
 def test():
     assert decimal_to_binary(0) == "0000000000000000"
